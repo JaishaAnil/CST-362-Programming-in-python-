e4e6321ae7defe4c21c5dcb7d6a491521b904d98
JAISHA <jaishaanil1010@gmail.com>
2026-01-05 23:57:11 +0530

    initialcommit

---
+++ desicion/password.py
@@ -0,0 +1,11 @@
+#8. Check password strength
+pwd = input("Enter password: ")
+
+l = len(pwd)
+
+if l < 6:
+    print("Weak")
+elif l <= 10:
+    print("Medium")
+else:
+    print("Strong")
---
+++ sequential problem/add.py
@@ -0,0 +1,4 @@
+#2. Add any 2 numbers and print the sum
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
